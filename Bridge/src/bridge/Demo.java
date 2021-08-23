4294dcba8179991f42d584aafd36ad102b059bcb
Alejandro Platas <alejandro.platas.02@gmail.com>
2021-08-23 17:59:10 -0500

    patrones de diseño

---
+++ Bridge/src/bridge/Demo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridge;
+
+import devices.Device;
+import devices.Radio;
+import devices.Tv;
+import remotes.AdvancedRemote;
+import remotes.BasicRemote;
+
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
---
+++ State/src/state/Demo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package state;
+
+import ui.Player;
+import ui.UI;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
